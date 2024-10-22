9b7b346afc9ce7209d887674abe19256c009a505
Igincan <igino97@gmail.com>
2024-10-22 23:41:22 +0200

    the cube is rotating

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,20 @@ const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight,
 const renderer = new WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x27213C);
 document.getElementById("app")?.appendChild(renderer.domElement);
 
 const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial({ color: 0x00ff00 });
+const material = new MeshBasicMaterial({ color: 0xA5FFD6 });
 const cube = new Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
-function animate() {
+renderer.setAnimationLoop(() => {
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
     renderer.render(scene, camera);
-}
-renderer.setAnimationLoop(animate);
+});
